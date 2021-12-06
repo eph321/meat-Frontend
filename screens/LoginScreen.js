ca6672e0d9b5e055c65a580b96f7a4ef2fcb8528
Etienne GARNIER <etienne.garnier@yahoo.fr>
2021-12-07 00:10:43 +0100

    update of work

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -2,20 +2,23 @@ import React, {useState} from 'react'
 
 import { EyeInvisibleOutlined, EyeTwoTone } from '@ant-design/icons';
 import  {StyleSheet, ImageBackground,View,Image} from "react-native";
-import { Button, Input } from 'react-native-elements'
+import {Button, Input, SocialIcon} from 'react-native-elements'
 import Icon from 'react-native-vector-icons/FontAwesome';
+import {TextInput} from "react-native-paper";
 
 function LoginScreen(props) {
     const [pseudo, setPseudo ] = useState("");
     const [password, setPassword ] = useState("");
 
     return (
-        <View>
+        <View style={styles.container}>
+
             <Image
-                style={styles.container}
+                style={styles.imageLogin}
                 source={require('../assets/loginBackground.jpg')}
-                resizeMode={"cover"}
+
             />
+
 
             <Input
                 containerStyle={{  width: '70%' }}
@@ -48,6 +51,7 @@ function LoginScreen(props) {
 
                 title="Connection"
                 type="solid"
+                containerStyle={{  width: '70%' }}
                 buttonStyle={{ backgroundColor: "#009788" }}
                 onPress={() => {  props.navigation.navigate('Home') }}
             />
@@ -55,9 +59,15 @@ function LoginScreen(props) {
 
                 title="Register"
                 type="solid"
+                containerStyle={{  width: '70%' }}
                 buttonStyle={{ backgroundColor: "#009788" }}
                 onPress={() => {  props.navigation.navigate('Register') }}
             />
+            <SocialIcon  button  light  type='google'
+                         containerStyle={{  width: '70%' }}
+                         title="Connection avec Google"/>
+            <SocialIcon  button  light  type='facebook'
+                         title="Connection avec Facebook"/>
         </View>
 
 
@@ -74,5 +84,13 @@ const styles = StyleSheet.create({
         flex: 1,
         alignItems: 'center',
         justifyContent: 'center',
-    },
+    },imageLogin :{
+        alignItems:"flex-start",
+        justifyContent:"flex-start",
+        flex: 1,
+        width: '100%',
+        height: "33%",
+        resizeMode: 'cover',
+        marginTop: 25
+    }
 });
