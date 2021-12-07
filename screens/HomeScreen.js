5767ecb3b93127060877b2aaa76f2d62684b9a04
Etienne GARNIER <etienne.garnier@yahoo.fr>
2021-12-08 00:19:49 +0100

    fix typos on HomeScreen.js

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,9 +1,33 @@
 import React, { useState } from 'react';
 import { StyleSheet, View } from 'react-native';
-import {Text, Button, Appbar} from "react-native-paper";
+import {Text, Button, Appbar,TextInput} from "react-native-paper";
+import DateTimePicker from '@react-native-community/datetimepicker';
 
 
 function HomeScreen(props) {
+    // DATE PICKER - input "où"
+    const [date, setDate] = useState(new Date(1598051730000));
+    const [mode, setMode] = useState('date');
+    const [show, setShow] = useState(false);
+
+    const onChange = (event, selectedDate) => {
+        const currentDate = selectedDate || date;
+        setShow(Platform.OS === 'ios');
+        setDate(currentDate);
+    };
+
+    const showMode = (currentMode) => {
+        setShow(true);
+        setMode(currentMode);
+    };
+
+    const showDatepicker = () => {
+        showMode('date');
+    };
+
+    const showTimepicker = () => {
+        showMode('time');
+    };
 
     return (   <View style={{flex:1,justifyContent: 'space-evenly'}}>
                     <View style={styles.viewHeader}>
@@ -26,7 +50,35 @@ function HomeScreen(props) {
                             mode="contained" onPress={() =>{ props.navigation.navigate('JoinTable'); }}>
                             <Text Style={{color:'#FFC960'}}>Go to join</Text>
                         </Button>
+                        <TextInput
+                            label="Où ?"
+                            placeholder="Paris 17"
+                        />
+                        <View>
+                            <TextInput
+                                label="Quand ?"
+                                placeholder="JJ/MM/AAAA"
+                                onFocus={showDatepicker}
+                            />
+                            {show && (
+                                <DateTimePicker
+                                    testID="dateTimePicker"
+                                    value={date}
+                                    mode={mode}
+                                    is24Hour={true}
+                                    display="default"
+                                    onChange={onChange}
+                                />
+                            )}
+
+                        </View>
+                        <TextInput
+                            label="De quoi avez-vous envie ?"
+                            placeholder="Italien"
+                        />
                     </View>
+
+
         </View>
 
     );
