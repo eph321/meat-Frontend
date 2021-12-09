4da22a68bb253947facd49cbfa9f24b1f920f1ac
Etienne GARNIER <etienne.garnier@yahoo.fr>
2021-12-09 15:46:25 +0100

    test

--- reducers/event.js
+++ reducers/event.js
@@ -1,5 +1,5 @@
 export default function (tableId = "", action) {
-    if (action.type == "saveTableId") {
+    if (action.type === "saveTableId") {
         return action.tableId
     } else {
         return tableId
